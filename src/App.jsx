ba1c105c9dcfa4743a6f1e078921c1fdf07da686
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-07-08 03:09:13 +0500

    Random Color Generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Accordian from "./components/accordian/accordian";
+import RandomColor from "./components/randomColor/randomColor";
 
 const App = () => {
   return (
     <div>
-      <Accordian />
+      {/* <Accordian /> */}
+      <RandomColor />
     </div>
   );
 };
