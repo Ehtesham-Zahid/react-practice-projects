00c7e453ba8cf998e92691ed13605d86dd2db1e5
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-07-09 00:48:13 +0500

    Image Slider done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import Accordian from "./components/accordian/accordian";
 import RandomColor from "./components/randomColor/randomColor";
 import StarRating from "./components/starRating/starRating";
+import ImageSlider from "./components/imageSlider/imageSlider";
 
 const App = () => {
   return (
     <div>
       {/* <Accordian /> */}
       {/* <RandomColor /> */}
-      <StarRating noOfStars={10} />
+      {/* <StarRating noOfStars={10} /> */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 };
