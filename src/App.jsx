3b723db81ddd901d953c582034d737e24d2e7c8e
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-07-08 23:25:47 +0500

    Star Rating

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Accordian from "./components/accordian/accordian";
 import RandomColor from "./components/randomColor/randomColor";
+import StarRating from "./components/starRating/starRating";
 
 const App = () => {
   return (
     <div>
       {/* <Accordian /> */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      <StarRating noOfStars={10} />
     </div>
   );
 };
